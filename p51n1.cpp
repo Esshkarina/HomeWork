aac81a7fa1718fd1bb7b6f35c719f3d295cf4018
Esshkarina <32554042+Esshkarina@users.noreply.github.com>
2017-10-06 10:26:11 +0300

    Update p51n1.cpp

--- p51n1.cpp
+++ p51n1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main () {
-    cout << "My name is Lisa." << "\tI live in Nizni Novgorod, Kazanskoe shosse street, 12b." << endl;
+    cout << "My name is Lisa." << "\tI live in Nizni Novgorod, Kazanskoe shosse street, 12/5." << endl;
 
 return 0;
 }
